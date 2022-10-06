09d315425635484b78f4d0d1be0667cb6222670c
jihyeonC <109576183+jihyeonC@users.noreply.github.com>
2022-10-07 00:30:24 +0900

    changing language

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
+/* Write your MySQL query statement below */
 select w1.id Id
 from weather w1 cross join weather w2
 where w1.recordDate-1 =w2.recordDate
